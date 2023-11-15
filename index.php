6085201b326e78922c87607766b8e8891ff47bb4
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-15 08:27:40 +0700

    create header

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php include "templates/head.php"; ?>
 <body>
 	<?php include "templates/header.php"; ?>
-	nguyen dang khoa - hoc vien cong nghe buu chin vien thonf
+
+	<div class="common-page-content-wrapper">
+		<div class="container">
+
+		<?php include "templates/subHeader.php"; ?>
+		</div>
+	</div>
 	<?php include "templates/footer.php"; ?>
+	<script>
+		$(function () {
+        // PC
+      $(".list-slider-image").slick({
+          speed: 600,
+          autoplaySpeed: 4000,
+          autoplay: true,
+          infinite: true,
+          swipe: true,
+          fade: false,
+          dots: false,
+          arrows: false,
+          slidesToShow: 1,
+          // variableWidth: true,
+          adaptiveHeight: true,
+          slidesToScroll: 1,
+        });
+      });
+	</script>
 </body>
 </html>
---
+++ templates/subHeader.php
@@ -0,0 +1,91 @@
+<!-- SLIDER BANNER  -->
+<div class="common-slider-banner">
+  <ul class="list-slider-image">
+    <li class="slider-image-item">
+      <a href="#">
+        <img
+          src="../images/slider-banner-image-3.png"
+          class="slider-image"
+          alt=""
+        />
+      </a>
+    </li>
+    <li class="slider-image-item">
+      <a href="#">
+        <img
+          src="../images/slider-banner-image-2.png"
+          class="slider-image"
+          alt=""
+        />
+      </a>
+    </li>
+    <li class="slider-image-item">
+      <a href="#">
+        <img
+          src="../images/slider-banner-image-3.png"
+          class="slider-image"
+          alt=""
+        />
+      </a>
+    </li>
+  </ul>
+</div>
+<!-- SLIDER BANNER  -->
+
+<!-- SUB MENU + SEARCH  BLOCK -->
+<div class="common-sub-menu">
+  <ul class="sub-menu">
+    <li class="menu-item live-menu">
+      <a href="#" class="live-item" title="" target="">
+        <div class="status-btn-wrapper">
+          <div class="livenow">
+            <div></div>
+          </div>
+          <span class="live-lable">LIVE</span>
+        </div>
+      </a>
+    </li>
+
+    <li class="menu-item">
+      <a href="#" title="" target="">
+        <img src="../images/football.png" class="icon-menu" alt="" />
+        <span class="txt-menu">Football</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#" title="" target="">
+        <img src="../images/tennis.png" class="icon-menu" alt="" />
+        <span class="txt-menu">Tennis</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#" title="" target="">
+        <img src="../images/basketball.png" class="icon-menu" alt="" />
+        <span class="txt-menu">Basketball</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#" title="" target="">
+        <img src="../images/ice-hockey.png" class="icon-menu" alt="" />
+        <span class="txt-menu">Other</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#" title="" target="">
+        <img src="../images/icon-calendar.png" class="icon-menu" alt="" />
+        <span class="txt-menu">Fixtures</span>
+      </a>
+    </li>
+  </ul>
+  <form action="" method="" class="search-block">
+    <input
+      type="text"
+      name=""
+      id=""
+      placeholder="Keyword..."
+      class="input-search"
+    />
+    <button type="submit" class="btn-search">Search</button>
+  </form>
+</div>
+<!-- SUB MENU + SEARCH  BLOCK -->
